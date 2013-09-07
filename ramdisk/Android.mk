1804e2750d34c581d58976c3bb49cabbbb1e9220
Ankur Raikwar <ankur5technical@gmail.com>
2013-09-07 17:23:57 +0530

    fix fstab
    remove unecesarry tags

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.msm7x27
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE       := fstab.$(SAMSUNG_BOOTLOADER)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.msm7x27
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -11,7 +10,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := charging.rle
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/charging.rle
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -19,7 +17,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.$(SAMSUNG_BOOTLOADER).rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.msm7x27.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -27,7 +24,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.$(SAMSUNG_BOOTLOADER).bluez.rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.msm7x27.bluez.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -35,7 +31,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.$(SAMSUNG_BOOTLOADER).parts.rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.msm7x27.parts.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -43,7 +38,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.$(SAMSUNG_BOOTLOADER).usb.rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.msm7x27.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -51,7 +45,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.$(SAMSUNG_BOOTLOADER).rc
-LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.msm7x27.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
